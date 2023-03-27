a71ed89775c9ff304e7cd8c0f3d8ff5f0a4ea2b2
Agnal <agnal.pindiyan@msds.christuniversity.in>
2023-03-27 20:52:30 +0530

    code

---
+++ LAB6/lab6.java
@@ -0,0 +1,159 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+class student{
+    String regno;
+    String name;
+    String email;
+    int phone;
+    String Class;
+    String department;
+     
+    student(String regno,String name,String email,int phone,String Class,String department){
+        this.regno=regno;
+        this.name=name;
+        this.email=email;
+        this.phone=phone;
+        this.Class=Class;
+        this.department=department;
+    }
+    void printDetails(){
+        System.out.println("Reg no:"+regno);
+        System.out.println("Name:"+name);
+        System.out.println("Email:"+email);
+        System.out.println("Phone:"+phone);
+        System.out.println("Class:"+Class);
+        System.out.println("Department:"+department);
+    }
+    void printStudent(){
+        System.out.println(name +"\t"+Integer.toString(phone));
+    
+
+    }  
+
+    void display(){
+        System.out.println(this.regno + "|"+this.name+"|"+this.email+"|"+this.phone+"|"+this.Class+"|"+this.department);
+    
+ }
+ boolean search(String name,String regno){
+    boolean result=(name.equalsIgnoreCase(name))&&(regno.equalsIgnoreCase(regno));
+    if(result){
+        printDetails();
+    }
+    return result;
+    }
+    }
+
+
+    
+
+
+public class lab6 {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        
+        student[] Stdetails=new student[100];
+        int ch; 
+        int count=0;
+
+        
+        do{
+            System.out.println("Enter 1:to add student,2:search for a student,3:display details,4:to end the program");
+            
+            ch=Integer.parseInt(scan.nextLine());
+            
+
+            switch(ch){
+                case 1:
+                System.out.println("Enter the Reg no:");
+                 String regno=scan.nextLine();
+        
+                System.out.println("Enter your name");
+                String name=scan.nextLine();
+        
+                System.out.println("Enter your email");
+                String email=scan.nextLine();
+        
+
+                System.out.println("Enter your phone number");
+                String phone=scan.nextLine();
+       
+                System.out.println("Enter your class");
+                String Class=scan.nextLine();
+        
+                System.out.println("Enter your Department");
+               String department=scan.nextLine();
+               Stdetails[count]=new student(regno,name,email,phone,Class,department);
+                
+                count+=1;
+                break;
+
+                case 2:
+                System.out.println("Enter the regno of the student you want to search");
+                String reg=scan.nextLine();
+                System.out.println("Enter the name of the student you want to search");
+                String name1=scan.nextLine();
+                boolean found=false;
+                for(int i=0;i<count;i++){
+                    if(Stdetails[i].search(name1,reg))
+                  {
+                    found=true;
+                    break;
+                }}
+                if(!found){
+                    System.out.println("Student not found");
+
+                }break;
+                
+                case 3:
+                for(int i=0;i<count;i++){
+                    Stdetails[i].printDetails();
+                }
+                break;
+
+                case 4:
+                System.out.println("end");
+                break;  
+            }
+           }
+           while(ch!=4);
+           try{
+            File file=new File("name.txt");  
+            if(file.exists()){
+                System.out.println("File exixts..");
+
+            }else{
+                System.out.println("File is missing");
+                if(file.createNewFile()){
+                    System.out.println("File created");
+                }else{
+                    System.out.println("File creation error");
+                }
+            }
+            
+            FileWriter write =new FileWriter(file);
+            write.write("File write");
+            write.close();
+
+        
+            Scanner scan = new Scanner(file);
+            while(scan.hasNextLine()){
+                System.out.println(scan.nextLine()); 
+            }
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+}
+
+           
+
+    
+    
+
+
+        
+    
+
