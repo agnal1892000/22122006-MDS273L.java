6ce3c5f6726668a280a65b694d91040680860712
Agnal <agnal.pindiyan@msds.christuniversity.in>
2023-05-12 09:24:26 +0530

    code

--- LAB1/lab1.java
+++ LAB1/lab1.java
@@ -1,43 +1,48 @@
+
 import java.util.Scanner;
-public class lab1{
-    public static void main(String[]args){
-        Scanner scan=new Scanner(System.in);
-        System.out.println("Enter the name:");
-        String Name=scan.nextLine();
-       System.out.println("Enter the gender-m/f:");
-        char Gender=scan.next().charAt(0);
-        scan.nextLine();
-        System.out.println("Enter the company name:");
-        String Company=scan.nextLine();
-        System.out.println("Enter the state name:");
-        String State=scan.nextLine();
-        if(Gender=='f'){
-            System.out.println("Employee gender-FEMALE");  
-        }
-        else if(Gender=='m'){
-            System.out.println("Employee gender-MALE");
-        }
-        switch(Company.toLowerCase()){
-            case "facebook":
-            case  "google":
-            case "microsoft":
-            case "samsung":
-            case "ibm":
-            case "apple":
-            System.out.println("The employee is working in Top MNC Companies");
-            break;
-            default:
-            break;
-        }
-          switch(State.toLowerCase()){
+
+public class lab1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Enter employee name: ");
+        String name = scanner.nextLine();
+        
+        System.out.print("Enter employee age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine(); 
+        
+        System.out.print("Enter employee salary: ");
+        double salary = scanner.nextDouble();
+        scanner.nextLine(); 
+        
+        System.out.print("Enter employee gender (m/f): ");
+        char gender = scanner.nextLine().charAt(0);
+        
+        System.out.print("Enter employee state : ");
+        String state = scanner.nextLine();
+        
+        System.out.println("Employee name: " + name);
+        System.out.println("Employee age: " + age);
+        System.out.println("Employee salary: " + salary);
+        System.out.println("Employee gender: " + (gender == 'm' ? "MALE" : "FEMALE"));
+        System.out.println("Employee state : " + state);
+        
+        
+        switch (state.toLowerCase()) {
+            case "andhra pradesh":
+            case "karnataka":
             case "kerala":
             case "tamil nadu":
-            case "karnataka":
             case "telangana":
-            case "andhra pradesh":
-            System.out.println("The Employee is from the southern states of India; Preferable work location is in  Andhra Pradesh, Karnataka, Kerala, Tamil Nadu and Telangana");
+            
+                System.out.println("The Employee is from the southern states of India;Preferable work location  " + state);
+                break;
+            default:
+                
         }
-        switch(State.toLowerCase()){
+        switch(state.toLowerCase()){
             case "jammu and kashmir":
             case "himachal Pradesh":
             case "punjab":
@@ -47,9 +52,9 @@ public class lab1{
             case "rajasthan":
             case "uttar pradesh":
             case "chandigarh":
-            System.out.println("The Employee is from the northern states of India; Preferable work location is in Jammu & Kashmir, Himachal Pradesh, Punjab, Uttarakhand, Haryana, Delhi, Rajasthan and Uttar Pradesh and Chandigarh.");
+            System.out.println("The Employee is from the northern states of India; Preferable work location "+state);
         }
-         switch(State.toLowerCase()){
+        switch(state.toLowerCase()){
             case "nagaland":
             case "manipur":
             case "meghalaya":
@@ -61,15 +66,25 @@ public class lab1{
             case "bihar":
             case "jarkhand":
             case "orissa":
-            System.out.println("The Employee is from the western states of India; Preferable work location is incase nagaland, manipur, meghalaya,tripura,arunachal pradesh,assam,mizoram,sikkim,bihar,jarkhand,orissa"); 
+            System.out.println("The Employee is from the western states of India; Preferable work location  "+state); 
         }
-        switch(State.toLowerCase()){
+        switch(state.toLowerCase()){
         case "gujarat":
         case "rajasthan":
         case "goa":
         case "west bengal":
         
-        System.out.println("The Employee is from the eastern states of India; Preferable work location is in gujarat , rajasthan ,goa ,west bengal");
+        System.out.println("The Employee is from the eastern states of India; Preferable work location "+state);
+        }
+        
+        
+        String[] topMncCompanies = {"Facebook", "Google", "Microsoft", "Samsung", "IBM", "Apple"};
+        for (String company : topMncCompanies) {
+            if (name.contains(company)) {
+                System.out.println("The employee is working in Top MNC Companies");
+                break;
+            }
         }
     }
 }
+
